24b8477ed8815b6a80f7b0958c4161bb7c6db34f
Bryan Personal <bryanlohjw97@gmail.com>
2026-05-15 19:33:12 +1000

    fix: for errors in actions

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+  },
+];
